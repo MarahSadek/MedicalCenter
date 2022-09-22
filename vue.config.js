65359af1721911df8bb25fcc0a2094783fd5e0f0
Marah <marah.sadeq@gmail.com>
2022-09-22 22:48:45 +0300

    all

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
-export default defineConfig(
-    {
-      base: '/mc/'
-    }
-)
+module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+};
